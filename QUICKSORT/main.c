7fabc11bab8113794a8aa5d8b96c2aea0e823243
Esau Higor <esauhigor@gmail.com>
2026-02-10 07:18:25 -0400

    criando as funções de particionar e o quicksort

--- QUICKSORT/main.c
+++ QUICKSORT/main.c
@@ -8,6 +8,34 @@ void troca(int *a, int *b){
     *b = temp;
 }
 
+int particionar(int lista[], int inicio, int fim){
+    int indicePivo = fim;
+    int i = inicio -1;
+    int j = inicio;
+
+    while(j < fim){
+        if(lista[j]< lista[indicePivo]){
+            i++;
+            troca(&lista[j], &lista[i]);
+        }
+        j++;
+    }
+    i++;
+    troca(&lista[i], &lista[indicePivo]);
+    return i;
+
+}
+
+void quicksort(int lista[], int inicio, int fim){
+    if(inicio < fim){
+        int indicePivo = particionar(lista, inicio, fim);
+        quicksort(lista, inicio, indicePivo-1);
+        quicksort(lista, indicePivo+1, fim);
+    }
+}
+
+
+
 
 int main(){
 
@@ -15,6 +43,19 @@ int main(){
     printf("%d, %d\n", x, y);
     troca(&x,&y);
     printf("%d, %d\n", x, y);
+
+    int list[] = {4 ,8, 9, 9, 9, 3, 2, 5};
+    for(int i=0; i<8 ; i++){
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+
+    quicksort(list, 0, 7);
+
+    for(int i=0; i<8 ; i++){
+        printf("%d ", list[i]);
+    }
+    printf("\n");
 
 
     return 0;
